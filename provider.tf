7048ae67e2151cca08e1b5ca794c65fc505b8b22
BenedictGithubacc <fbenedictwesly@gmail.com>
2025-04-17 11:17:47 +0530

    changes

--- provider.tf
+++ provider.tf
@@ -5,9 +5,21 @@ terraform {
       version = "~> 4.0"
     }
   }
+  
+  backend "remote" {
+    hostname = "domain.scalr.io"
+    organization = "domain"
+
+    workspaces {
+      name = "new-modules"
+    }
+  }
 }
+
+
+
 
 # Configure the AWS Provider
 provider "aws" {
-  region =var.aws_region
+  region = var.aws_region
 }
